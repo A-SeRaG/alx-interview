5c0c45547cdf7a0ee31453c1ff649c33b04f089c
SeRaG <aserag0101@gmail.com>
2024-10-17 21:05:14 +0300

    Done

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+"""
+method that calculates the fewest number of operations
+needed to result in exactly n H characters in the file
+"""
+
+
+def minOperations(n):
+    """
+    Minimum Operations method
+
+    Args:
+        n: number of operations
+
+    Returns:
+        integer & If n is impossible to achieve, return 0
+    """
+    if n < 2:
+        return 0
+
+    operations = 0
+
+    divisor = 2
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
+
+    return operations
