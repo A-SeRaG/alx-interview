690e3a95c64c06dd6b5a6517e756fa630aa37503
SeRaG <aserag0101@gmail.com>
2024-10-31 17:59:03 +0300

    doc

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
+"""
+method that determines if a given data set
+represents a valid UTF-8 encoding.
+"""
+
 
 def validUTF8(data):
     """
-    method that determines if a given data set
-    represents a valid UTF-8 encoding.
+    check validUTF8
+
+    Args:
+        data: given bainary data
+
+    Returns: True or Folase
     """
     num_bytes = 0
 
